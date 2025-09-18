ba17234a246e85f43a1fafc14a495620bfbd6444
JesusCardoza <jesusdousquie99@gmail.com>
2025-09-17 20:49:44 -0500

    Taller completado

---
+++ TallerJdbc/src/App/ConexionDB.java
@@ -0,0 +1,32 @@
+package App;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class ConexionDB {
+
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/universidad?serverTimezone=UTC&useSSL=false&allowPublicKeyRetrieval=true";
+
+
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.err.println("verifico si el jbdc esta conectado , en la lib");
+            e.printStackTrace();
+        }
+    }
+
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
